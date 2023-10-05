f6266794b0fe68ffcccba2f5b58be5dcdbc93b88
Beata Ambroziak <beataxambroziak@gmail.com>
2023-10-05 13:24:37 +0200

    blocking form submission

--- src/Form/index.js
+++ src/Form/index.js
@@ -1,12 +1,18 @@
 import "./style.css"
 
-const Form = ({children, annotation}) => (
-    <form className="form">
-    <fieldset className="form__fieldset">
-      {children}
-    </fieldset>
-    <p className="from__annotation">{annotation}</p>
-  </form>
-)
+const Form = ({ children, annotation}) => {
+  const onFormSubmit = (event) => {
+    event.preventDefault();
+  };
+
+  return (
+    <form className="form" onSubmit={onFormSubmit}>
+      <fieldset className="form__fieldset">
+        {children}
+      </fieldset>
+      <p className="from__annotation">{annotation}</p>
+    </form>
+  )
+};
 
 export default Form;
