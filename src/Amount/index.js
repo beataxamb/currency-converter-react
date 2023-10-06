6c0d86a20552b6f0cd62b1180b8f04b2d0af14aa
Beata Ambroziak <beataxambroziak@gmail.com>
2023-10-06 13:26:49 +0200

    Controlled component "Amount"

--- src/Amount/index.js
+++ src/Amount/index.js
@@ -1,13 +1,17 @@
 import "./style.css"
 
-const Amount = () => (
-    <input
-        className="amount"
-        min="10"
-        step="0.01"
-        type="number"
-        placeholder="Wpisz kwotę w zł"
-    />
-);
+const Amount = ({ value, onChange }) => {
+    return (
+        <input
+            className="amount"
+            min="10"
+            step="0.01"
+            type="number"
+            placeholder="Wpisz kwotę w zł"
+            value={value}
+            onChange={onChange}
+        />
+    );
+};
 
 export default Amount;
--- src/App.js
+++ src/App.js
@@ -5,11 +5,46 @@ import Amount from "./Amount";
 import Label from "./Label";
 import Header from "./Header";
 import Form from "./Form"
+import { useState } from "react";
 
 function App() {
+
+  const [amount, setAmount] = useState("");
+  const [currency, setCurrency] = useState("EUR");
+  const [result, setResult] = useState("N/A");
+
+  const onInputChange = ({ target }) => {
+    setAmount(target.value);
+  };
+
+  const onSelectChange = ({ target }) => {
+    setCurrency(target.value);
+  };
+
+  const calculateResult = () => {
+    const calculateResult = calculate();
+    setResult(calculateResult);
+  };
+
+  const calculate = () => {
+    switch (currency) {
+      case "EUR":
+        return amount * 0.21;
+
+      case "GBP":
+        return amount * 0.19;
+
+      case "USD":
+        return amount * 0.22;
+
+      default:
+        return "N/A";
+    }
+  }
+
   return (
     <Form
-    annotation="*-pole wymagane"
+      annotation="*-pole wymagane"
     >
       <Header
         title="Kalkulator walut"
@@ -17,17 +52,17 @@ function App() {
       <Label
         title="Kwota w złotówkach:* "
         formField={
-          <Amount />
+          <Amount value={amount} onChange={onInputChange} />
         }
       />
       <Label
         title="Wybierz walutę: "
         formField={
-          <Currency />
+          <Currency value={currency} onChange={onSelectChange} />
         }
       />
-      <Buttons />
-      <Result />
+      <Buttons calculateResult={calculateResult} />
+      <Result result={result} currency={currency} />
     </Form>
   );
 }
